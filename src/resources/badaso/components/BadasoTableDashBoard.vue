1f7f724cfa173e6bc7eb3eb760a2b7ed218e9d88
Jetro Mobilha <jetromobilha@gmail.com>
2023-03-02 05:39:33 +0100

    adicionanando widget tabela

--- src/resources/badaso/components/BadasoTableDashBoard.vue
+++ src/resources/badaso/components/BadasoTableDashBoard.vue
@@ -17,25 +17,26 @@
             :description-connector="$t('crudGenerated.footer.descriptionConnector')"
             :description-body="$t('crudGenerated.footer.descriptionBody')"
           >
-            <template slot="thead"
-              v-for="(dataRow, index) in dataType.dataRows"
-              :key="index"
-              :sort-key="$caseConvert.stringSnakeToCamel(dataRow.field)"
-            >
-              <vs-th v-if="dataRow.browse == 1">
-                {{ dataRow.displayName }}
+            <template slot="thead">
+              <vs-th
+                v-for="(dataRow, index) in dataType.dataRows"
+                :key="index"
+                :sort-key="$caseConvert.stringSnakeToCamel(dataRow.field)"
+              >
+                <template v-if="dataRow.browse == 1">
+                  {{ dataRow.displayName }}
+                </template>
               </vs-th>
               <vs-th> {{ $t("crudGenerated.header.action") }} </vs-th>
             </template>
 
-            <template slot-scope="{ data }"
-              :data="record"
-              :key="index"
-              v-for="(record, index) in data"
-              :state="idsOfflineDeleteRecord.includes(record.id.toString())?'danger':'default'"
-            >
+            <template slot-scope="{ data }">
               <vs-tr
-                >
+                :data="record"
+                :key="index"
+                v-for="(record, index) in data"
+                :state="idsOfflineDeleteRecord.includes(record.id.toString())?'danger':'default'"
+              >
                 <template
                   v-if="!idsOfflineDeleteRecord.includes(record.id.toString()) || !isOnline"
                 >
