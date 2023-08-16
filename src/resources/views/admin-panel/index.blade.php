c481e56513e34e89c1d1f09176f5c214d0b99ddc
JetroMobilha <jetromobilha@gmail.com>
2023-08-16 16:44:41 +0100

    alterar o caminho dos activos na pagina princepal

--- src/resources/views/admin-panel/index.blade.php
+++ src/resources/views/admin-panel/index.blade.php
@@ -24,7 +24,7 @@
         <link rel="shortcut icon" href="{{ asset('storage' . '/' . $favicon) }}" type="image/png">
     @endif
 
-    @vite('style.scss')
+    @vite('vendor/bamfura/core/src/resources/badaso/assets/scss/style.scss')
 </head>
 <body>
     <div id="app"></div>
@@ -33,6 +33,7 @@
         window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
         window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
     </script>
-     @vite('app.js')
+    @vite('vendor/bamfura/core/src/resources/badaso/app.js')
+
 </body>
 </html>
