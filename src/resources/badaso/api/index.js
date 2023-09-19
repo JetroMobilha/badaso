2dd29f944c30c6f5b30e69f7d0c1c58f89c662e0
JetroMobilha <jetromobilha@gmail.com>
2023-09-19 06:08:32 +0100

    remover bug de importação de arquivo de api

--- src/resources/badaso/api/index.js
+++ src/resources/badaso/api/index.js
@@ -42,7 +42,7 @@ try {
       .replace(/resources\/js\/badaso\/api\//, "")
       .replace("./", "")
       .replace(/\.\.\//g, "")
-      .replace(/\./g, "")
+      .replace(/^\./g, "")
       .replace(".js", "")
       .replace(/([a-z])([A-Z])/g, "$1-$2") // get all lowercase letters that are near to uppercase ones
       .replace(/[\s_]+/g, "-") // replace all spaces and low dash
