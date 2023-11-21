a7730752dbc2bbaa781c4edbbd660500cf74b148
JetroMobilha <jetromobilha@gmail.com>
2023-11-21 16:09:08 +0100

    adicionar model UserCalgrupo

---
+++ src/Models/UserCalGrupo.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Uasoft\Badaso\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class UserCalGrupo extends Model
+{
+
+    public $table = "user_calgrupos";
+  
+}
