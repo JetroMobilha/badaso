a792301fc070e1a42eb0af154f8b7c906bd547cd
JetroMobilha <jetromobilha@gmail.com>
2023-11-22 04:48:50 +0100

    adicionar model UserCalevento

---
+++ src/Models/UserCalevento.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Uasoft\Badaso\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class UserCalevento extends Model
+{
+
+    public $table = "user_caleventos";
+  
+}
