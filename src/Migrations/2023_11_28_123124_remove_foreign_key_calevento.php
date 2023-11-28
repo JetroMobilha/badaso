6a98f68bb517cf629623783e60b8c2d555a5b7a2
JetroMobilha <jetromobilha@gmail.com>
2023-11-28 13:39:23 +0100

    remover chave estrangeria da tabela calevento de categorias

---
+++ src/Migrations/2023_11_28_123124_remove_foreign_key_calevento.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('caleventos', function (Blueprint $table) {
+            $table->dropForeign(['categoria_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
